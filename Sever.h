feccb4765363dc2890fb46f5b41065161c09f71e
tian ss git <814552122@qq.com>
2024-03-31 16:49:02 +0800

    1.增加了初始化监听的套接字方法

---
+++ Sever.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// 初始化监听的套接字
+int initListenFd(uint16_t port);
---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+  
+    if (argc < 3) {
+        printf("port path\n");
+        return -1;
+    }
+    unsigned short int  port = atoi(argv[1]);
+    // 切换服务器工作目录
+    chdir(argv[2]);
+    // 初始化用于监听的套接字
+    int lfd = initListenFd(10000);
+
+    // 启动服务程序
+    
+
+
+    return 0;
+}
